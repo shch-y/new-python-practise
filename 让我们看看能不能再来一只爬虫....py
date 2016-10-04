ff8013adefe74174a1a4abf5d56fe37701effc21
shichengyu999 <shichengyu999@yahoo.com>
2016-10-04 21:26:17 +0800

    Update 让我们看看能不能再来一只爬虫....py
    what a bad day! I don't want to touch lxml any more...

--- 让我们看看能不能再来一只爬虫....py
+++ 让我们看看能不能再来一只爬虫....py
@@ -1,6 +1,25 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-#
-import lxml
+#by scy in 10.4.lt's a practise! 
+
+
+
+import sys
+reload(sys)
+sys.setdefaultencoding('utf8')   #finish code problme
+
+import urllib2,urllib,re,os,lxml
+import lxml.etree as etree
+
+#catch html
+re1 = urllib2.urlopen("http://top.baidu.com/").read()
+print re1
+
+
+
+re1 = lxml.etree.HTML(re1)
+print re1 
+re2 = re1.xpath('/html/body/div[@class="wrappr"]/div[@id="main"]/div[@class="row"][1]/div[@class="box-hot tab"]/div[@class="tab-bd"]/div[@class="tab-box"]/ul')
+print re2
 
